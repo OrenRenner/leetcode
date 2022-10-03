2aa678b80686f4780f7f3d758f677e320ed7e37d
Emil Nishanov <emil.nishanov@advncd.com>
2022-10-03 09:34:02 +0500

    03.10.2022: 771; 804; 1365; 1512; 1656; 2006; 2325; 2367; 2418.

---
+++ HashTable/1512_NumberOfGoodPairs.py
@@ -0,0 +1,42 @@
+"""
+
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+
+https://ru.wikipedia.org/wiki/%D0%A5%D0%B5%D1%88-%D1%82%D0%B0%D0%B1%D0%BB%D0%B8%D1%86%D0%B0
+
+"""
+
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        res = 0
+        for i, item_i in enumerate(nums):
+            for j, item_j in enumerate(nums):
+                if nums[i] == nums[j] and i < j:
+                    res += 1
+        return res
---
+++ HashTable/2418_SortThePeople.py
@@ -0,0 +1,51 @@
+"""
+
+You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+
+For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+Return names sorted in descending order by the people's heights.
+
+
+
+Example 1:
+
+Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+Output: ["Mary","Emma","John"]
+Explanation: Mary is the tallest, followed by Emma and John.
+Example 2:
+
+Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
+Output: ["Bob","Alice","Bob"]
+Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
+
+
+Constraints:
+
+n == names.length == heights.length
+1 <= n <= 10^3
+1 <= names[i].length <= 20
+1 <= heights[i] <= 10^5
+names[i] consists of lower and upper case English letters.
+All the values of heights are distinct.
+
+"""
+
+
+class Solution:
+    def sortPeople(self, names: list, heights: list) -> list:
+        name_height_dict = {}
+        for index, name in enumerate(names):
+            name_height_dict[heights[index]] = name
+
+        names.clear()
+        while len(name_height_dict.keys()):
+            names.append(
+                name_height_dict.pop(
+                    max(name_height_dict.keys())
+                )
+            )
+        return names
+
+a = Solution()
+print(a.sortPeople(names = ["Mary","John","Emma"], heights = [180,165,170]))
