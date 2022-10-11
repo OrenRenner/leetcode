b5c18d4c205b88a51f12ce000a884db1bd269d06
Emil Nishanov <emil.nishanov@advncd.com>
2022-10-11 10:39:39 +0500

    11.10.2022: 2206; 2363.

--- HashTable/2206_DivideArrayIntoEqualPairs.py
+++ HashTable/2206_DivideArrayIntoEqualPairs.py
@@ -13,7 +13,7 @@ Return true if nums can be divided into n pairs, otherwise return false.
 Example 1:
 
 Input: nums = [3,2,3,2,2,2]
-Output: true
+Output: true 
 Explanation: 
 There are 6 elements in nums, so they should be divided into 6 / 2 = 3 pairs.
 If nums is divided into the pairs (2, 2), (3, 3), and (2, 2), it will satisfy all the conditions.
--- HashTable/2363_MergeSimilarItems.py
+++ HashTable/2363_MergeSimilarItems.py
@@ -14,7 +14,7 @@ Example 1:
 
 Input: items1 = [[1,1],[4,5],[3,8]], items2 = [[3,1],[1,5]]
 Output: [[1,6],[3,9],[4,5]]
-Explanation: 
+Explanation:  
 The item with value = 1 occurs in items1 with weight = 1 and in items2 with weight = 5, total weight = 1 + 5 = 6.
 The item with value = 3 occurs in items1 with weight = 8 and in items2 with weight = 1, total weight = 8 + 1 = 9.
 The item with value = 4 occurs in items1 with weight = 5, total weight = 5.  
